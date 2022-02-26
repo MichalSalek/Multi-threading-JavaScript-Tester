7eaf589f5354235969d3dfc34137b65d3c3897cf
MichalSalek <contact@michalsalek.pl>
2022-02-26 17:46:13 +0100

    Start page information

--- src/pages/start.tsx
+++ src/pages/start.tsx
@@ -5,6 +5,7 @@ import { ROUTE_MAIN_APP_SCREEN } from '@/core/routes.core'
 import { setStorageItem } from '@/features/background/browser-storage/browserStorage.api'
 import { STORAGE_KEY_START_PAGE_SEEN } from '@/app-config-constants'
 import AppButtonAtom from '@/app-components/AppButton.atom'
+import { Typography } from '@mui/material'
 
 
 
@@ -16,11 +17,20 @@ const StartScreenPage: NextPage = () => {
         return () => undefined
     })
 
-    return (
+    return (<main>
+
+        <br/>
+
+        <Typography variant="h5"> Work in progress: Building application UI.</Typography>
+        <Typography variant="h6" component="p"> Please come back soon to feel a new look.</Typography>
+
+        <br/>
+
         <Link passHref href={ROUTE_MAIN_APP_SCREEN}>
-            <AppButtonAtom>go to {ROUTE_MAIN_APP_SCREEN} page...</AppButtonAtom>
+            <AppButtonAtom>Go to main app page – this way</AppButtonAtom>
         </Link>
-    )
+
+    </main>)
 }
 
 export default StartScreenPage
