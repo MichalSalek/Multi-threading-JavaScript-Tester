74a40f883229e94776f403afbeb07f30a897a57e
MichalSalek <contact@michalsalek.pl>
2022-02-26 16:13:23 +0100

    without PWA build test

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 import path from 'path'
-import withPWA from 'next-pwa'
-import runtimeCaching from 'next-pwa/cache.js'
 
 
-export default withPWA({
-    pwa: {
-        dest: 'public',
-        runtimeCaching,
-    },
+export default {
     reactStrictMode: true,
     sassOptions: {
         includePaths: [path.join(path.dirname('./src'), 'styles')]
@@ -17,4 +11,4 @@ export default withPWA({
         locales: ["en"],
         defaultLocale: "en",
     }
-})
+}
