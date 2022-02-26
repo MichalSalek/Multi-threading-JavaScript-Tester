46d9b4abf7a183ba4b1ec2df358490c8276e2bb0
MichalSalek <contact@michalsalek.pl>
2022-02-26 16:44:50 +0100

    new link in NavLinks, deploy test through by script

--- src/features/building/nav-links/UI/NavLinks.molecule.tsx
+++ src/features/building/nav-links/UI/NavLinks.molecule.tsx
@@ -1,4 +1,4 @@
-import { ROUTE_MAIN_APP_SCREEN } from '@/core/routes.core'
+import { ROUTE_MAIN_APP_SCREEN, ROUTE_START_PAGE_SCREEN } from '@/core/routes.core'
 import AppButtonAtom from '@/app-components/AppButton.atom'
 import Link from 'next/link'
 import React from 'react'
@@ -8,8 +8,8 @@ import React from 'react'
 const NavLinksMolecule = (): JSX.Element => {
 
     return <nav>
-        <Link passHref href={ROUTE_MAIN_APP_SCREEN}>
-            <AppButtonAtom>go to {ROUTE_MAIN_APP_SCREEN} page...</AppButtonAtom>
+        <Link passHref href={ROUTE_START_PAGE_SCREEN}>
+            <AppButtonAtom>go to {ROUTE_START_PAGE_SCREEN} page...</AppButtonAtom>
         </Link>
         <Link passHref href={ROUTE_MAIN_APP_SCREEN}>
             <AppButtonAtom>go to {ROUTE_MAIN_APP_SCREEN} page...</AppButtonAtom>
