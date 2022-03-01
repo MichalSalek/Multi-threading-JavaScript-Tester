4ec003d9e7dc432a84df6d5ffac6dcad5c1a650c
MichalSalek <contact@michalsalek.pl>
2022-03-01 16:36:32 +0100

    global style bugfix (nextjs config)

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 import withPWA from 'next-pwa'
 import runtimeCaching from 'next-pwa/cache.js'
+import path from 'path'
+import {fileURLToPath} from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const NextJSAppSettings = {
     reactStrictMode: true,
     sassOptions: {
-        includePaths: ['./src/global-styles']
+        includePaths: [path.join(__dirname, 'global-styles')],
     },
     i18n: {
         locales: ["en"],
