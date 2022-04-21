9800f8e0b4f77be20d96253fd14b578bcd01bdfc
MichalSalek <contact@michalsalek.pl>
2022-04-22 01:04:55 +0200

    bumping up packages versions + lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,34 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true,
-        'node': true
+    root: true,
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    'extends': [
+    extends: [
         'eslint:recommended',
         'plugin:react/recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:jsx-a11y/recommended',
         'prettier',
-        'next/core-web-vitals'
+        'next/core-web-vitals',
     ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        'ecmaVersion': 13,
-        'sourceType': 'module'
+        ecmaVersion: 13,
+        sourceType: 'module',
     },
-    'plugins': [
-        'react',
-        '@typescript-eslint',
-        'prettier'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ],
-        'import/no-anonymous-default-export': [
-            'off'
-        ],
-        'react-hooks/exhaustive-deps': [
-            'error'
-        ]
-    }
+    plugins: ['@typescript-eslint', 'react'],
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'windows'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+        'import/no-anonymous-default-export': ['off'],
+        'react-hooks/exhaustive-deps': ['error']
+    },
 }
