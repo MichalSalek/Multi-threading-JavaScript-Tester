1a27753a2b4b130cf0dcba4d6cc5d36517ef0e44
MichalSalek <contact@michalsalek.pl>
2023-01-09 21:59:09 +0100

    AppButton parametrization for efunctionality extend

--- src/app-components/AppButton.atom.tsx
+++ src/app-components/AppButton.atom.tsx
@@ -1,17 +1,37 @@
-import React from 'react'
-import { Button } from '@mui/material'
-import { ButtonProps } from '@mui/material/Button/Button'
+import React, {useState} from 'react'
+import {Button} from '@mui/material'
+import {ButtonProps} from '@mui/material/Button/Button'
 import scss from './AppComponents.module.scss'
+import {freezeThreadAndWait} from '@/coding-utils/asyncOperations.api'
 
 
 
-const AppButtonAtom = (props: ButtonProps): JSX.Element => {
+type Props = {
+  freezeOnClick?: boolean
+}
+
+const AppButtonAtom = (props: ButtonProps & Props): JSX.Element => {
+
+    const [initialSettingsButtonDisabledState, setInitialSettingsButtonDisabledState] = useState(false)
+
+    const disableTemporary = async () => {
+        setInitialSettingsButtonDisabledState(true)
+        await freezeThreadAndWait(450)
+        setInitialSettingsButtonDisabledState(false)
+    }
 
     return <Button
         color={'primary'}
         variant={'contained'}
         className={scss.button}
-        {...props} />
+        {...props}
+        disabled={props.disabled || initialSettingsButtonDisabledState}
+        onClick={(event) => {
+            props.freezeOnClick && disableTemporary()
+
+            props.onClick && props.onClick(event)
+        }}
+    />
 }
 
 export default AppButtonAtom
